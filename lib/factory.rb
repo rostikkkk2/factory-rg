66eeaf85b0eda4f93418c4def7c0800be6e73bf7
rostikkkk2 <rost4402255@gmail.com>
2018-11-22 17:14:18 +0200

    ready methods

--- lib/factory.rb
+++ lib/factory.rb
@@ -10,14 +10,12 @@ class Factory
 
     def class_new(*args, &block)
       Class.new do
-        attr_accessor(*args)
+        attr_reader(*args)
 
         define_method :initialize do |*vals|
           raise ArgumentError.new unless args.count == vals.count
           args.zip(vals).each {|key, val| instance_variable_set("@#{key}", val)}
         end
-
-        class_eval(&block) if block_given?
 
         def ==(obj)
           self.class == obj.class && self.map_instance == obj.map_instance
@@ -26,10 +24,60 @@ class Factory
         def [](val)
           (val.is_a? Integer) ? map_instance[val] : instance_variable_get("@#{val}")
         end
-        
+
+        def []= (ins, val)
+          instance_variable_set("@#{ins}", val)
+        end
+
+        def dig(*values)
+          values.inject(self) do |key, value|
+            return nil if key[value].nil?
+            key[value]
+          end
+        end
+
+        def each(&block)
+          map_instance.each(&block)
+        end
+
+        def each_pair(&block)
+          to_h.each(&block)
+        end
+
+        def length
+          instance_variables.length
+        end
+
+        def members
+          to_h.map {|key, val| key.to_sym}
+        end
+
+        def select(&block)
+          map_instance.select(&block)
+        end
+
+        def to_a
+          map_instance
+        end
+
+        def values_at(*nums)
+          nums.map {|val| map_instance[val]}
+        end
+
+        def to_h()
+          result = {}
+          instance_variables.map { |n| result[n[1..-1]] = instance_variable_get(n) }
+          result
+        end
+
         def map_instance
           instance_variables.map {|var| instance_variable_get(var)}
         end
+
+        class_eval(&block) if block_given?
+
+        alias_method :eql, :==
+        alias_method :size, :length
 
       end
     end
--- spec/factory_spec.rb
+++ spec/factory_spec.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 require_relative '../lib/factory'
 
@@ -57,98 +56,98 @@ RSpec.describe 'Factory' do
 
     joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
 
-    # expect(joe['name']).to eq('Joe Smith')
-    # expect(joe[:name]).to eq('Joe Smith')
+    expect(joe['name']).to eq('Joe Smith')
+    expect(joe[:name]).to eq('Joe Smith')
     expect(joe[0]).to eq('Joe Smith')
   end
 
-  # it 'attribute assignment operator works as expected' do
-  #   Customer = Factory.new(:name, :address, :zip)
-  #
-  #   joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
-  #
-  #   joe['name'] = 'Luke'
-  #   joe[:zip]   = '90210'
-  #
-  #   expect(joe.name).to eq('Luke')
-  #   expect(joe.zip).to eq('90210')
-  # end
-  #
-  # it 'dig works as expected' do
-  #   Customer = Factory.new(:a)
-  #
-  #   c = Customer.new(Customer.new(b: [1, 2, 3]))
-  #
-  #   expect(c.dig(:a, :a, :b, 0)).to eq(1)
-  #   expect(c.dig(:b, 0)).to be_nil
-  #
-  #   expect { c.dig(:a, :a, :b, :c) }.to raise_error(TypeError)
-  # end
-  #
-  # it 'each works as expected' do
-  #   Customer = Factory.new(:name, :address, :zip)
-  #
-  #   joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
-  #
-  #   each_elements = []
-  #
-  #   joe.each { |x| each_elements << x }
-  #
-  #   expect(each_elements).to match_array(['Joe Smith', '123 Maple, Anytown NC', 12_345])
-  # end
-  #
-  # it 'each_pair works as expected' do
-  #   Customer = Factory.new(:name, :address, :zip)
-  #
-  #   joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
-  #
-  #   each_elements = []
-  #
-  #   joe.each_pair { |name, value| each_elements << "#{name} => #{value}" }
-  #
-  #   expect(each_elements).to match_array(['name => Joe Smith', 'address => 123 Maple, Anytown NC', 'zip => 12345'])
-  # end
-  #
-  # it 'length (size) works as expected' do
-  #   Customer = Factory.new(:name, :address, :zip)
-  #
-  #   joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
-  #
-  #   expect(joe.length).to eq(3)
-  #   expect(joe.size).to eq(3)
-  # end
-  #
-  # it 'members works as expected' do
-  #   Customer = Factory.new(:name, :address, :zip)
-  #
-  #   joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
-  #
-  #   expect(joe.members).to match_array(%i[name address zip])
-  # end
-  #
-  # it 'selects works as expected' do
-  #   Customer = Factory.new(:a, :b, :c, :d, :e, :f)
-  #
-  #   l = Customer.new(11, 22, 33, 44, 55, 66)
-  #
-  #   result = l.select(&:even?)
-  #
-  #   expect(result).to match_array([22, 44, 66])
-  # end
-  #
-  # it 'to_a works as expected' do
-  #   Customer = Factory.new(:name, :address, :zip)
-  #
-  #   joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
-  #
-  #   expect(joe.to_a[1]).to eq('123 Maple, Anytown NC')
-  # end
-  #
-  # it 'values_at works as expected' do
-  #   Customer = Factory.new(:name, :address, :zip)
-  #
-  #   joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
-  #
-  #   expect(joe.values_at(0, 2)).to eq(['Joe Smith', 12_345])
-  # end
+  it 'attribute assignment operator works as expected' do
+    Customer = Factory.new(:name, :address, :zip)
+
+    joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
+
+    joe['name'] = 'Luke'
+    joe[:zip]   = '90210'
+
+    expect(joe.name).to eq('Luke')
+    expect(joe.zip).to eq('90210')
+  end
+
+  it 'dig works as expected' do
+    Customer = Factory.new(:a)
+
+    c = Customer.new(Customer.new(b: [1, 2, 3]))
+
+    expect(c.dig(:a, :a, :b, 0)).to eq(1)
+    expect(c.dig(:b, 0)).to be_nil
+
+    expect { c.dig(:a, :a, :b, :c) }.to raise_error(TypeError)
+  end
+
+  it 'each works as expected' do
+    Customer = Factory.new(:name, :address, :zip)
+
+    joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
+
+    each_elements = []
+
+    joe.each { |x| each_elements << x }
+
+    expect(each_elements).to match_array(['Joe Smith', '123 Maple, Anytown NC', 12_345])
+  end
+
+  it 'each_pair works as expected' do
+    Customer = Factory.new(:name, :address, :zip)
+
+    joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
+
+    each_elements = []
+
+    joe.each_pair { |name, value| each_elements << "#{name} => #{value}" }
+
+    expect(each_elements).to match_array(['name => Joe Smith', 'address => 123 Maple, Anytown NC', 'zip => 12345'])
+  end
+
+  it 'length (size) works as expected' do
+    Customer = Factory.new(:name, :address, :zip)
+
+    joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
+
+    expect(joe.length).to eq(3)
+    expect(joe.size).to eq(3)
+  end
+
+  it 'members works as expected' do
+    Customer = Factory.new(:name, :address, :zip)
+
+    joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
+
+    expect(joe.members).to match_array(%i[name address zip])
+  end
+
+  it 'selects works as expected' do
+    Customer = Factory.new(:a, :b, :c, :d, :e, :f)
+
+    l = Customer.new(11, 22, 33, 44, 55, 66)
+
+    result = l.select(&:even?)
+
+    expect(result).to match_array([22, 44, 66])
+  end
+
+  it 'to_a works as expected' do
+    Customer = Factory.new(:name, :address, :zip)
+
+    joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
+
+    expect(joe.to_a[1]).to eq('123 Maple, Anytown NC')
+  end
+
+  it 'values_at works as expected' do
+    Customer = Factory.new(:name, :address, :zip)
+
+    joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
+
+    expect(joe.values_at(0, 2)).to eq(['Joe Smith', 12_345])
+  end
 end
